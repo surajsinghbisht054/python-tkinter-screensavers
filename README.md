148e6b4131fbbd5a960bbff632057f6a3547c117
Suraj Singh Bisht <surajsinghbisht054@gmail.com>
2017-04-09 00:39:26 +0530

    Latest Update

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # python-tkinter-screensavers
 Screen Saver App Written In Python and Tkinter GUI
+```
+######################################################
+                By S.S.B Group                          
+######################################################
+
+    Suraj Singh
+    Admin
+    S.S.B Group
+    surajsinghbisht054@gmail.com
+    http://bitforestinfo.blogspot.in/
+
+    Note: We Feel Proud To Be Indian
+######################################################
+```
+
+# ScreenShot 1
+
+![Pipe Screenshot 1](scr/test.png?raw=true "Python-pipe_screensaver.py")
+
+# Screenshot 2
+
+![Pipe Screenshot 2](scr/test1.png?raw=true "Python-pipe_screensaver.py")
---
+++ pipes/tkinter_pipes_screensaver.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+
+# ---------------- READ ME ---------------------------------------------
+# This Script is Created Only For Practise And Educational Purpose Only
+# This is an Example Of Tkinter Canvas Graphics
+# This Script Is Created For http://bitforestinfo.blogspot.in
+# This Script is Written By
+#
+#
+##################################################
+######## Please Don't Remove Author Name #########
+############### Thanks ###########################
+##################################################
+#
+#
+__author__='''
+
+######################################################
+                By S.S.B Group                          
+######################################################
+
+    Suraj Singh
+    Admin
+    S.S.B Group
+    surajsinghbisht054@gmail.com
+    http://bitforestinfo.blogspot.in/
+
+    Note: We Feel Proud To Be Indian
+######################################################
+'''
+#print __author__
+
+
+# ============ CONFIGURATIONS =====================
+SCREENSAVER_BACKGROUND_COLOR="black"
+TRANSPARENCY_LEVEL = 0.3
+COLOR_CHOICE = ['blue',"red","yellow","white","skyblue","green"]
+SCREEN_SAVER_CLOSING_EVENTS = ['<Any-KeyPress>', '<Any-Button>', '<Motion>']
+PIPE_WIDTH = 10
+WAITING_TIME_LAP = 0.09
+RESTARTING_POINT = 100
+LOOK_LIKE_THREAD = True
+# =================================================
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ===================== Scripts Starts From Here =============================+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# *************** Importing Module ****************
+try:
+    import Tkinter, random, time
+except:
+    import tkinter as Tkinter, random, time
+
+
+# Create Canvas Class
+class Pipes(Tkinter.Canvas):
+    def __init__(self, *args, **kwargs):
+        Tkinter.Canvas.__init__(self, *args, **kwargs)
+        self.coordinates=[0,0,0,0]
+        self.create_pipe()
+
+    # Create A Line
+    def create_pipe(self):
+        self.p = self.create_line(0,0,0,0, fill=random.choice(COLOR_CHOICE),smooth=LOOK_LIKE_THREAD ,width=PIPE_WIDTH)
+        return
+
+    # update Canvas Configurations
+    def update_screen(self):
+        self.coordinates.append(random.randint(0,self.winfo_screenwidth()))
+        self.coordinates.append(random.randint(0,self.winfo_screenheight()))
+        self.coords(self.p, *self.coordinates)
+        time.sleep(WAITING_TIME_LAP)
+        if len(self.coordinates)>RESTARTING_POINT:
+            self.coordinates=[0,0,0,0]
+            color = random.choice(COLOR_CHOICE)
+            self.itemconfigure(self.p, fill=color)
+        return
+
+# Main Functions
+def main():
+    root=Tkinter.Tk(className="Pipes screenSaver By Bitforestinfo")
+    screen = Pipes(root, bg=SCREENSAVER_BACKGROUND_COLOR)
+    screen.pack(expand="yes",fill="both")
+
+    root.wait_visibility(screen)
+    root.wm_attributes('-alpha',TRANSPARENCY_LEVEL)
+    root.wm_attributes("-topmost", True)
+    root.overrideredirect(1)
+    root.attributes('-fullscreen', True)
+
+    def out(event):
+        root.destroy()
+        return
+    
+    for seq in SCREEN_SAVER_CLOSING_EVENTS:
+        root.bind_all(seq, out)
+
+    while True:
+        root.update()
+        root.update_idletasks()
+        screen.update_screen()
+    return
+
+
+#============ Trigger ========================  
+if __name__=='__main__':
+    main()
